7c8314ed2caf343e909329716be6a9f8aa106e28
akktii <aktanmoldokeev123@gmail.com>
2025-08-01 16:44:09 +0600

    install i18next & basic usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useTranslation } from "react-i18next";
+import "./App.css";
+
 function App() {
-  return <>i18next project</>;
+  const { t, i18n } = useTranslation();
+
+  const handleChangeLanguage = () => {
+    const newLanguage = i18n.language === "ru" ? "en" : "ru";
+
+    i18n.changeLanguage(newLanguage);
+  };
+
+  return (
+    <div className="container">
+      <h1>{t("text.hello")}</h1>
+      <h2>{t("text.yourName", { name: "Актан" })}</h2>
+      <button onClick={handleChangeLanguage}>{t("changeLang")}</button>
+    </div>
+  );
 }
 
 export default App;
